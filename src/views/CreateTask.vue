57318f8840337ee4c0e15061e608bceda845be17
Timmothy Nguyen <tnguyenthaison50@gmail.com>
2024-10-07 14:15:14 +0200

    initial commit

---
+++ src/views/CreateTask.vue
@@ -0,0 +1,22 @@
+<template>
+<div class="form-container">
+  <input placeholder="Title"/>
+  <input placeholder="Description"/>
+</div>
+</template>
+
+<style scoped>
+
+.form-container {
+
+}
+
+</style>
+
+<script setup lang="ts">
+
+
+
+
+
+</script>
